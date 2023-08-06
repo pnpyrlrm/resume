71f9f13b75bb98181febe358f14b4c2c1bf9f3e7
pnpyrlrm <dv56026@gmail.com>
2023-08-06 17:09:55 +0300

    end 8 task

--- src/route/index.js
+++ src/route/index.js
@@ -41,7 +41,9 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: 'index',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
